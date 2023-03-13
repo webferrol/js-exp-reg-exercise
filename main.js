55f4874117251cbd33322c1d8b7ddbb534d1f702
webferrol <xurxo@webferrol.com>
2023-03-13 15:21:05 +0100

    Refactorización 2: Cambie los if's por un switch

--- main.js
+++ main.js
@@ -9,34 +9,36 @@ function manipularBoton (documentPadre) {
   const input = documentPadre.querySelector('input')
   const name = input.getAttribute('name')
 
-  if (name === 'campo') {
-    const expReg = /^\s*$/
-    if (expReg.test(input.value)) console.log('El campo nombre no puede estar vacío')
-  }
+  let expReg
 
-  if (name === 'numero-positivo') {
-    const expReg = /^\+?\d+$/
-    if (!expReg.test(input.value)) console.log('Sólo se admiten números positivos')
-  }
-
-  if (name === 'nif') {
-    const expReg = /^\d{8}-?[a-zA-Z]$/
-    if (!expReg.test(input.value)) console.log('NIF NO VÁLIDO')
-  }
-
-  if (name === 'cp') {
-    const expReg = /^[0-5]\d{4}$/
-    if (!expReg.test(input.value)) console.log('cp NO VÁLIDO')
-  }
-
-  if (name === 'login') {
-    const expReg = /^[\w]+$/i
-    if (!expReg.test(input.value)) console.log('login NO VÁLIDO')
-  }
-
-  if (name === 'fecha') {
-    const expReg = /^([0-3][0-9]\/[0-1][0-9]\/\d{4})|([0-3][0-9]-[0-1][0-9]-\d{4})$/
-    if (!expReg.test(input.value)) console.log('fecha NO VÁLIDO')
+  switch (name) {
+    case 'campo':
+      expReg = /^\s*$/
+      if (expReg.test(input.value)) console.log('El campo nombre no puede estar vacío')
+      break
+    case 'numero-positivo':
+      expReg = /^\+?\d+$/
+      if (!expReg.test(input.value)) console.log('Sólo se admiten números positivos')
+      break
+    case 'nif':
+      expReg = /^\d{8}-?[a-zA-Z]$/
+      if (!expReg.test(input.value)) console.log('NIF NO VÁLIDO')
+      break
+    case 'cp':
+      expReg = /^[0-5]\d{4}$/
+      if (!expReg.test(input.value)) console.log('cp NO VÁLIDO')
+      break
+    case 'login':
+      expReg = /^[\w]+$/i
+      if (!expReg.test(input.value)) console.log('login NO VÁLIDO')
+      break
+    case 'fecha':
+      expReg = /^([0-3][0-9]\/[0-1][0-9]\/\d{4})|([0-3][0-9]-[0-1][0-9]-\d{4})$/
+      if (!expReg.test(input.value)) console.log('fecha NO VÁLIDO')
+      break
+    default:
+      console.log('No existe ese campo')
+      break
   }
 }
 
